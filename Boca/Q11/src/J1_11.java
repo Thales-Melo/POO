a23e28827761a43a05af44fb4b5bc2813bc8ff63
thales-penha <thales.penha@edu.ufes.br>
2024-04-10 14:58:24 -0300

    Q11 loading...

---
+++ Boca/Q11/src/J1_11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class J1_11 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        scanner.nextLine(); // Despreza o \n.
+        CarrinhoDeCompras c = new CarrinhoDeCompras(num);
+        for (int i = 0; i < num; i++) {
+            String nome = scanner.nextLine();
+            double preco = scanner.nextDouble();
+            scanner.nextLine(); // Despreza o \n.
+            int qtd = scanner.nextInt();
+            scanner.nextLine(); // Despreza o \n.
+            c.adicionar(nome, preco, qtd);
+        }
+
+
+        scanner.close();
+    }
+}
