b80b14409b4f99dad240e713ae99ba327445d8c9
thales-penha <thales.penha@edu.ufes.br>
2024-05-07 16:25:15 -0300

    Q2 done

--- Boca/JAVA_3/Q2/src/J3_02.java
+++ Boca/JAVA_3/Q2/src/J3_02.java
@@ -1,5 +1,40 @@
-public class J3_02 {
-    public static void main(String[] args) throws Exception {
+import java.io.IOException;
+
+public class J3_02 extends Exception {
+
+    public static void metodo01() throws ClassNotFoundException {
+        Class.forName("ClasseQueNaoExiste");
+    }
+    public static void metodo02() throws IOException{
+        java.io.File.createTempFile("pre", "suf");
+    }
+    public static void metodo03() throws Exception {
+        Integer.class.newInstance();
+    }
+    public static void main(String[] args) {
+
+        try {
+            metodo01();
+        } 
+        catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage() + " nao existe!");
+        }
+        
+        try {
+            metodo02();
+        } 
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        
+        try {
+            metodo03();
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        }    
+        
         
     }
 }
